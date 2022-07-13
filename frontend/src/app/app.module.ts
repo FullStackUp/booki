54a1cc32b7cde27b9fff5a0bc33d8452cb5a9292
FullStackUp <fullstackup.webdeveloper@gmail.com>
2022-07-14 01:38:38 +0200

    Add footer component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, BodyComponent],
+  declarations: [AppComponent, HeaderComponent, BodyComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
