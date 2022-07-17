7c171306267b91b7df99df9edc6b0619cb9d361e
FullStackUp <fullstackup.webdeveloper@gmail.com>
2022-07-18 00:02:29 +0200

    General view update

--- frontend/src/app/body/body.component.ts
+++ frontend/src/app/body/body.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faInfo } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
-  styleUrls: ['./body.component.scss']
+  styleUrls: ['./body.component.scss'],
 })
 export class BodyComponent implements OnInit {
+  infoIcon = faInfo;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
